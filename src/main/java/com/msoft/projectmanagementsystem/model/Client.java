b0e3b30f536d790b039faa538457ca6cc56c2202
Diwash <dopkhrel111@gmail.com>
2025-01-02 17:36:17 +0545

    added models

---
+++ src/main/java/com/msoft/projectmanagementsystem/model/Client.java
@@ -0,0 +1,9 @@
+package com.msoft.projectmanagementsystem.model;
+
+/**
+ * Created On : 2025 02 Jan 5:29 PM
+ * Author : Diwash Pokhrel
+ * Description:
+ **/
+public class Client {
+}
---
+++ src/main/java/com/msoft/projectmanagementsystem/model/Permissions.java
@@ -0,0 +1,9 @@
+package com.msoft.projectmanagementsystem.model;
+
+/**
+ * Created On : 2025 02 Jan 5:30 PM
+ * Author : Diwash Pokhrel
+ * Description:
+ **/
+public class Permissions {
+}
