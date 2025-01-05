437883053f3c0d1176e568396b661908f79dc48e
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-05 13:47:48 +0545

    Added logic for Client

--- src/main/java/com/msoft/projectmanagementsystem/model/Client.java
+++ src/main/java/com/msoft/projectmanagementsystem/model/Client.java
@@ -15,18 +15,11 @@ import java.time.LocalDateTime;
  * Author : Diwash Pokhrel
  * Description:
  **/
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, unique = true, length = 100)
-    private String username;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -64,4 +57,90 @@ public class Client {
         updatedAt = LocalDateTime.now();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getClientCompanyName() {
+        return clientCompanyName;
+    }
+
+    public void setClientCompanyName(String clientCompanyName) {
+        this.clientCompanyName = clientCompanyName;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Client() {
+    }
+
+    public Client(Long id, String email, String password, String firstName, String lastName, String clientCompanyName, Company company, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.clientCompanyName = clientCompanyName;
+        this.company = company;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
