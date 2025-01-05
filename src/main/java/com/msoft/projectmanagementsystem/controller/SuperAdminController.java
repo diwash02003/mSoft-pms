a15b710b4c848504f65e701d8a52b1ac26b79a60
Diwash <dopkhrel111@gmail.com>
2025-01-05 13:02:14 +0545

    made same function for update and create admin, also code is documented

--- src/main/java/com/msoft/projectmanagementsystem/controller/SuperAdminController.java
+++ src/main/java/com/msoft/projectmanagementsystem/controller/SuperAdminController.java
@@ -15,6 +15,7 @@ import java.util.List;
  * Created On : 2025 03 Jan 3:02 PM
  * Author : Diwash Pokhrel
  * Description:
+ * REST controller for managing Super Admins and Companies.
  **/
 @RestController
 @RequestMapping("/superadmin")
@@ -30,27 +31,38 @@ public class SuperAdminController {
         this.companyService = companyService;
     }
 
-    @PostMapping()
-    public ResponseEntity<SuperAdmin> createAdmin(@RequestBody SuperAdmin superAdmin) {
+    /**
+     * Creates a new Super Admin or updates an existing Super Admin.
+     *
+     * @param superAdmin the Super Admin details to be created or updated
+     * @return ResponseEntity containing the created or updated Super Admin and HTTP status
+     */
+    @PostMapping("/createOrUpdateAdmin")
+    public ResponseEntity<SuperAdmin> createOrUpdateAdmin(@RequestBody SuperAdmin superAdmin) {
         try {
-            SuperAdmin admin = superAdminService.createAdmin(superAdmin);
-            return new ResponseEntity<>(admin, HttpStatus.CREATED);
+            SuperAdmin result;
+
+            // Check if superAdminId is present (update case)
+            if (superAdmin.getSuperAdminId() != null) {
+                result = superAdminService.updateAdmin(superAdmin.getSuperAdminId(), superAdmin);
+            } else {
+                // Create new Super Admin (create case)
+                result = superAdminService.createAdmin(superAdmin);
+            }
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (Exception e) {
-            throw new customException("Error creating superAdmin: " + e.getMessage());
+            throw new customException("Error processing super admin information: " + e.getMessage());
         }
     }
 
-    @PutMapping("{/id}")
-    public ResponseEntity<SuperAdmin> updateSuperAdmin(@PathVariable Long id, @RequestBody SuperAdmin updateSuperAdmin) {
-        try {
-            SuperAdmin admin = superAdminService.updateAdmin(id, updateSuperAdmin);
-            return new ResponseEntity<>(admin, HttpStatus.OK);
-        } catch (Exception e) {
-            throw new customException("Error Updating super admin" + e.getMessage());
-        }
-    }
 
-
+    /**
+     * Creates a new Company or updates an existing Company.
+     *
+     * @param company the Company details to be created or updated
+     * @return ResponseEntity containing the created or updated Company and HTTP status
+     */
     @PostMapping("/createOrUpdateCompany")
     public ResponseEntity<Company> createOrUpdateCompany(@RequestBody Company company) {
         try {
@@ -66,6 +78,11 @@ public class SuperAdminController {
         }
     }
 
+    /**
+     * Retrieves a list of all companies.
+     *
+     * @return ResponseEntity containing the list of companies or no content if empty
+     */
     @GetMapping("/companies")
     public ResponseEntity<List<Company>> getAllCompanies() {
         List<Company> companies = companyService.getAllCompanies();
