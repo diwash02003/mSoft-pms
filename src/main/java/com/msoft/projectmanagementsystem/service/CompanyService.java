a3403e12d3d55c7ce1d1f251b483756a574468e7
Diwash <dopkhrel111@gmail.com>
2025-01-05 11:49:22 +0545

    keeping code in their

--- src/main/java/com/msoft/projectmanagementsystem/service/CompanyService.java
+++ src/main/java/com/msoft/projectmanagementsystem/service/CompanyService.java
@@ -1,6 +1,10 @@
 package com.msoft.projectmanagementsystem.service;
 
+import com.msoft.projectmanagementsystem.model.Company;
+import com.msoft.projectmanagementsystem.repo.CompanyRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created On:1/2/25 5:29 PM
@@ -9,4 +13,17 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class CompanyService {
+    private final CompanyRepo companyRepo;
+
+    public CompanyService(CompanyRepo companyRepo) {
+        this.companyRepo = companyRepo;
+    }
+
+    public Company createCompany(Company company) {
+        return companyRepo.save(company);
+    }
+
+    public List<Company> getAllCompanies() {
+        return companyRepo.findAll();
+    }
 }
--- src/main/java/com/msoft/projectmanagementsystem/service/SuperAdminService.java
+++ src/main/java/com/msoft/projectmanagementsystem/service/SuperAdminService.java
@@ -1,12 +1,8 @@
 package com.msoft.projectmanagementsystem.service;
 
-import com.msoft.projectmanagementsystem.model.Company;
 import com.msoft.projectmanagementsystem.model.SuperAdmin;
-import com.msoft.projectmanagementsystem.repo.CompanyRepo;
 import com.msoft.projectmanagementsystem.repo.SuperAdminRepo;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * Created On:1/2/25 5:28 PM
@@ -16,22 +12,13 @@ import java.util.List;
 @Service
 public class SuperAdminService {
     private final SuperAdminRepo superAdminRepo;
-    private final CompanyRepo companyRepo;
 
-    public SuperAdminService(SuperAdminRepo superAdminRepo, CompanyRepo companyRepo) {
+    public SuperAdminService(SuperAdminRepo superAdminRepo) {
         this.superAdminRepo = superAdminRepo;
-        this.companyRepo = companyRepo;
     }
 
     public SuperAdmin createAdmin(SuperAdmin superAdmin) {
         return superAdminRepo.save(superAdmin);
     }
 
-    public Company createCompany(Company company) {
-        return companyRepo.save(company);
-    }
-
-    public List<Company> getAllCompanies() {
-        return companyRepo.findAll();
-    }
 }
