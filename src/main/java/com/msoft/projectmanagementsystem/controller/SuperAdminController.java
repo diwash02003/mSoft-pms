b4ac4011538b169b84934921dc50ed5116e99940
Diwash <dopkhrel111@gmail.com>
2025-01-05 17:09:43 +0545

    diwash: added function for delete company by id

--- src/main/java/com/msoft/projectmanagementsystem/controller/SuperAdminController.java
+++ src/main/java/com/msoft/projectmanagementsystem/controller/SuperAdminController.java
@@ -97,4 +97,18 @@ public class SuperAdminController {
         return ResponseEntity.ok(companies);
     }
 
+    @DeleteMapping("deleteCompany/{companyId}")
+    public ResponseEntity<String> deleteCompanyById(@PathVariable Long companyId) {
+        try {
+            boolean isDeleted = companyService.deleteCompanyById(companyId);
+            if (isDeleted) {
+                return ResponseEntity.ok("Company deleted successfully");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Company not found");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while deleting company: " + e.getMessage());
+        }
+    }
+
 }
